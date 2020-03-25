8300f1abb96e7a65703c539ddb14b37b024dac29
Kevin Cui JY <kevincui1230@gmail.com>
2020-03-25 12:11:43 -0400

    Try point system

--- client.py
+++ client.py
@@ -211,7 +211,7 @@ async def tea(ctx, user=None):
     if user is None:
         with open('data/users.json') as f:
             data = json.load(f)
-        await ctx.send(ctx.message.author.mention + ' You have ' + str(data.get(ctx.message.author.id, 0)) + ' cups of :tea:.')
+        await ctx.send(ctx.message.author.mention + ' You have ' + str(data.get(str(ctx.message.author.id), 0)) + ' cups of :tea:.')
         return
     if not user[3:-1].isdigit():
         await ctx.send(ctx.message.author.mention + ' Invalid query. Please use format `%stea <user>`.' % prefix)
