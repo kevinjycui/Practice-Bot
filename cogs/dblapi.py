7fa87eabd028f13591ed863c45f302c096d69a1b
kevinjycui <info@kevincui.dev>
2020-06-25 18:07:12 -0400

    Debug DBL

--- cogs/dblapi.py
+++ cogs/dblapi.py
@@ -9,18 +9,18 @@ class DiscordBotLists(commands.Cog):
     def __init__(self, bot, bot_id, tokens):
         self.bot = bot
         self.data = tokens
-        self.data['bot_id'] = bot_id
+        self.data['bot_id'] = str(bot_id)
 
     @tasks.loop(minutes=30)
     async def post_guild_count(self):
         self.data['server_count'] = len(self.bot.guilds)
-        requests.post('https://botblock.org/api/count', data=json.dumps(self.data), headers={'Content-type':'application/json', 'Accept':'application/json'})
+        requests.post('https://botblock.org/api/count', json=self.data, headers={'Content-type':'application/json', 'Accept':'application/json'})
 
     @commands.command()
     @commands.is_owner()
     async def post_guild_count_manual(self, ctx):
         self.data['server_count'] = len(self.bot.guilds)
-        response = requests.post('https://botblock.org/api/count', data=json.dumps(self.data), headers={'Content-type':'application/json', 'Accept':'application/json'})
+        response = requests.post('https://botblock.org/api/count', json=self.data, headers={'Content-type':'application/json', 'Accept':'application/json'})
         print(response.status_code)
         print(response.json())
 
