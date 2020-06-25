70aa8bb807e1f9a3332950c447762baed8a485dc
kevinjycui <info@kevincui.dev>
2020-06-25 19:58:07 -0400

    Separate TOPGG calls

--- cogs/dblapi.py
+++ cogs/dblapi.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands, tasks
+import dbl
 import requests
 import json
 import sys
@@ -21,7 +22,20 @@ class DiscordBotLists(commands.Cog):
 
     @post_guild_count.before_loop
     async def post_guild_count_before(self):
-        await self.bot.wait_until_ready()      
+        await self.bot.wait_until_ready() 
+
+class TopGG(commands.Cog):
+    """Handles interactions with the top.gg API"""
+
+    def __init__(self, bot, token):
+        self.bot = bot
+        self.token = token
+        self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True) # Autopost will post your guild count every 30 minutes
+
+    async def on_guild_post():
+        print("Server count posted successfully")
 
 def setup(bot, bot_id, tokens):
+    bot.add_cog(TopGG(bot, tokens['top.gg']))
+    tokens.pop('top.gg')
     bot.add_cog(DiscordBotLists(bot, bot_id, tokens))
