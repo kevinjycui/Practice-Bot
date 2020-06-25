75ca8721671671751a2cdcef3fc59a4412cbdaba
kevinjycui <info@kevincui.dev>
2020-06-25 19:44:46 -0400

    Debug DBL

--- cogs/dblapi.py
+++ cogs/dblapi.py
@@ -15,15 +15,13 @@ class DiscordBotLists(commands.Cog):
     @tasks.loop(minutes=30)
     async def post_guild_count(self):
         self.data['server_count'] = len(self.bot.guilds)
-        requests.post('https://botblock.org/api/count', json=self.data, headers={'Content-type':'application/json', 'Accept':'application/json'})
-
-    @commands.command()
-    @commands.is_owner()
-    async def post_guild_count_manual(self, ctx):
-        self.data['server_count'] = len(self.bot.guilds)
         response = requests.post('https://botblock.org/api/count', json=self.data, headers={'Content-type':'application/json', 'Accept':'application/json'})
         with open('log.txt', 'w+') as log:
             print(response.json(), file=log)
+
+    @post_guild_count.before_loop
+    async def post_guild_count_before(self):
+        await self.bot.wait_until_ready()      
 
 def setup(bot, bot_id, tokens):
     bot.add_cog(DiscordBotLists(bot, bot_id, tokens))
