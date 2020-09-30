f6f966c08184b4cdb52fd1dafeeea0ccfdd6ea6e
kevinjycui <kevincui1230@gmail.com>
2020-09-29 21:26:28 -0400

    Correct timezone format

--- cogs/contests.py
+++ cogs/contests.py
@@ -118,7 +118,7 @@ class ContestCog(commands.Cog):
         if contests is not None:
             for contest in range(len(contests)):
                 name, details = list(contests.items())[contest]
-                if datetime.strptime(details['start_time'], '%Y-%m-%dT%H:%M:%S%z').timestamp() > time():
+                if datetime.strptime(details['start_time'].replace(':', ''), '%Y-%m-%dT%H%M%S%z').timestamp() > time():
                     spec = json_get('https://dmoj.ca/api/contest/info/' + name)
                     url = 'https://dmoj.ca/contest/' + name
                     contest_data = {
@@ -126,8 +126,8 @@ class ContestCog(commands.Cog):
                         'description': url,
                         'oj': 'dmoj',
                         'thumbnail': 'https://raw.githubusercontent.com/kevinjycui/Practice-Bot/master/assets/dmoj-thumbnail.png',
-                        'Start Time': datetime.strptime(details['start_time'], '%Y-%m-%dT%H:%M:%S%z').strftime('%Y-%m-%d %H:%M:%S%z'),
-                        'End Time': datetime.strptime(details['start_time'], '%Y-%m-%dT%H:%M:%S%z').strftime('%Y-%m-%d %H:%M:%S%z')
+                        'Start Time': datetime.strptime(details['start_time'].replace(':', ''), '%Y-%m-%dT%H%M%S%z').strftime('%Y-%m-%d %H:%M:%S%z'),
+                        'End Time': datetime.strptime(details['start_time'].replace(':', ''), '%Y-%m-%dT%H%M%S%z').strftime('%Y-%m-%d %H:%M:%S%z')
                     }
                     if details['time_limit']:
                         contest_data['Time Limit'] = details['time_limit']
