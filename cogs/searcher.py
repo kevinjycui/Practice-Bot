831321c495555adc8490f2dbdbcd6f82e651fad6
kevinjycui <kevincui1230@gmail.com>
2020-11-04 17:26:33 -0500

    Trim wiki summary

--- cogs/searcher.py
+++ cogs/searcher.py
@@ -49,21 +49,6 @@ class SearcherCog(commands.Cog):
         except:
             return False
 
-    def accountScrape(self, name):
-        accounts = {}
-        if self.valid('https://dmoj.ca/api/user/info/%s' % name):
-            accounts['DMOJ'] = 'https://dmoj.ca/user/%s' % name
-        cf_data = requests.get('https://codeforces.com/api/user.info?handles=%s' % name)
-        if cf_data.status_code == 200 and cf_data.json()['status'] == 'OK':
-            accounts['Codeforces'] = 'https://codeforces.com/profile/%s' % name
-        if self.valid('https://atcoder.jp/users/%s' % name):
-            accounts['AtCoder'] = 'https://atcoder.jp/users/%s' % name
-        if self.valid('https://wcipeg.com/user/%s' % name):
-            accounts['WCIPEG'] = 'https://wcipeg.com/user/%s' % name
-        if self.valid('https://github.com/%s' % name):
-            accounts['GitHub'] = 'https://github.com/%s' % name
-        return accounts
-
     def tag_visible(self, element):
         if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
             return False
@@ -97,7 +82,7 @@ class SearcherCog(commands.Cog):
             title, summary, url = peg_res
             embed = discord.Embed(title=title, description=url + ' (searched in %ss)' % str(round(self.bot.latency, 3)))
             embed.timestamp = datetime.utcnow()
-            embed.add_field(name='Summary', value=summary, inline=False)
+            embed.add_field(name='Summary', value=summary[-1024:], inline=False)
             await ctx.send(ctx.message.author.display_name + ', Here\'s what I found!', embed=embed)
             return
         page, summary = self.getSummary(name.replace(' ', '_'))
@@ -108,22 +93,6 @@ class SearcherCog(commands.Cog):
         embed.timestamp = datetime.utcnow()
         embed.add_field(name='Summary', value=summary[-1024:], inline=False)
         await ctx.send(ctx.message.author.display_name + ', Here\'s what I found!', embed=embed)
-
-    # @commands.command()
-    # async def whois(self, ctx, *, name=None):
-    #     if name is None:
-    #         prefix = await self.bot.command_prefix(self.bot, ctx.message)
-    #         await ctx.send(ctx.message.author.display_name + ', Invalid query. Please use format `%swhois <name>`.' % prefix)
-    #         return
-    #     accounts = self.accountScrape(name)
-    #     if len(accounts) == 0:
-    #         await ctx.send(ctx.message.author.display_name + ', Sorry, found 0 results for %s' % name)
-    #         return
-    #     embed = discord.Embed(title=name, description=' (searched in %ss)' % str(round(self.bot.latency, 3)))
-    #     embed.timestamp = datetime.utcnow()
-    #     for oj, url in accounts.items():
-    #         embed.add_field(name=oj, value=url, inline=False)
-    #     await ctx.send(ctx.message.author.display_name + ', Found %d result(s) for `%s`' % (len(accounts), name), embed=embed)
 
     @commands.command()
     async def cat(self, ctx):
